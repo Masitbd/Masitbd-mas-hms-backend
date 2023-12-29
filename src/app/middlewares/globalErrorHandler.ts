78d218d6d7e880311fb7abc85deea2b67c6599b0
shahidx0x <dev.imshahid@gmail.com>
2023-12-29 09:34:52 +0600

    update migration_lock.toml, schema.prisma, globalErrorHandler.ts and handleValidationError.ts

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -5,8 +5,10 @@ import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
 import config from '../../config';
 import ApiError from '../../errors/ApiError';
 
+import { Prisma } from '@prisma/client';
 import { ZodError } from 'zod';
 import handleCastError from '../../errors/handleCastError';
+import handleValidationError from '../../errors/handleValidationError';
 import handleZodError from '../../errors/handleZodError';
 import { IGenericErrorMessage } from '../../interfaces/error';
 import { errorlogger } from '../../shared/logger';
@@ -25,11 +27,11 @@ const globalErrorHandler: ErrorRequestHandler = (
   let message = 'Something went wrong !';
   let errorMessages: IGenericErrorMessage[] = [];
 
-  if (error?.name === 'ValidationError') {
-    // const simplifiedError = handleValidationError(error);
-    // statusCode = simplifiedError.statusCode;
-    // message = simplifiedError.message;
-    // errorMessages = simplifiedError.errorMessages;
+  if (error instanceof Prisma.PrismaClientValidationError) {
+    const simplifiedError = handleValidationError(error);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorMessages = simplifiedError.errorMessages;
   } else if (error instanceof ZodError) {
     const simplifiedError = handleZodError(error);
     statusCode = simplifiedError.statusCode;
--- src/errors/handleValidationError.ts
+++ src/errors/handleValidationError.ts
@@ -1,12 +1,14 @@
+import { Prisma } from '@prisma/client';
 import { IGenericErrorResponse } from '../interfaces/common';
 
 const handleValidationError = (
+  error: Prisma.PrismaClientValidationError
 ): IGenericErrorResponse => {
   const statusCode = 400;
   return {
     statusCode,
     message: 'Validation Error',
-    errorMessages: [],
+    errorMessages: [{ path: '', message: error.message }],
   };
 };
 
